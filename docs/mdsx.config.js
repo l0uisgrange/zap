6f79ed038d7af3dd351d639373fb65456504dd49
Louis <70532216+l0uisgrange@users.noreply.github.com>
2025-09-30 23:05:10 +0200

    fixed shiki highlighting

--- docs/mdsx.config.js
+++ docs/mdsx.config.js
@@ -1,21 +1,35 @@
 import { defineConfig } from 'mdsx';
 import { rehypeCustomHighlight } from '@mdsx/rehype-custom-highlighter';
-
+import { createHighlighterCore } from 'shiki/core';
+import { createJavaScriptRegexEngine } from 'shiki/engine/javascript';
+import * as typstLang from './src/lib/components/typst.tmLanguage.json' with { type: 'json' };
 
 /** @type {import('@mdsx/rehype-custom-highlighter').HighlightOptions} */
 const highlightOptions = {
 	highlight: async ({ value, lang, meta }) => {
-		// your highlighting logic here
-		return value;
-	},
+		if (!lang) return value;
+		if (!highlighter) {
+			highlighter = await highlighterPromise;
+		}
+		return highlighter.codeToHtml(value, { lang: 'typst', themes: { light: 'github-light', dark: 'github-dark' } });
+	}
 };
 
+const jsEngine = createJavaScriptRegexEngine();
+let highlighter = null;
+
+const highlighterPromise = createHighlighterCore({
+	themes: [import('@shikijs/themes/github-light'), import('@shikijs/themes/github-dark')],
+	langs: [typstLang],
+	engine: jsEngine
+});
+
 export const mdsxConfig = defineConfig({
-	blueprints: {
-		default: {
-			path: "src/lib/blueprints/default/blueprint.svelte",
-		},
-	},
 	extensions: ['.md'],
 	rehypePlugins: [[rehypeCustomHighlight, highlightOptions]],
+	blueprints: {
+		default: {
+			path: 'src/lib/blueprints/default/blueprint.svelte'
+		}
+	}
 });
